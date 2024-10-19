e1f984a692f2b2557a8b569be21a68a45139cf21
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-19 23:58:23 +0200

    add starting files and some base .h and .c

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 23:44:46 by estettle          #+#    #+#             */
+/*   Updated: 2024/10/19 23:57:48 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+// libraries
+# include <stdarg.h> // for variadic functions and va_ helpers
+
+// function prototypes
+int ft_printf(const char *, ...);
+
+#endif
